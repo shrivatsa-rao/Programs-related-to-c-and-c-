17469bcdfb841300e878e5e177890a25912da800
Shrivtsa <68243174+shrivatsa-rao@users.noreply.github.com>
2022-09-27 11:10:56 +0530

    Add files via upload

---
+++ mcq.c
@@ -0,0 +1,143 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+// int x=10;
+
+// int main()
+// {
+    
+// int x=0;
+// printf("%d",x);
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a@=10;
+//     printf("%d",a@);
+//     return 0;
+// }
+
+
+// int main()
+// {
+//     int i;
+//     for(int i=0;i<-5;i--)
+//     {
+//         printf("*");
+//     }
+    
+// }
+
+
+// int main()
+// {
+//     int i=0;
+//     while(i<=255)
+//     {
+//         printf("%d ",i);
+//         i++;
+//     }
+// }
+
+
+// int main()
+// {
+//     switch(20%13/2)
+//     {
+//         case 0:
+//         printf("case 0");
+//         case 1:
+//         printf("case 0");
+//         case 2:
+//         printf("case 0");
+//         default:
+//         printf("defal");
+        
+        
+//     }
+//     return 0;
+// }
+
+
+
+int main()
+{
+    // int var1=10;
+    // {
+    //     int var1=20;
+    //     printf("%d %d",var1,var2);
+        
+    // }
+    //   printf("%d %d",var1,var2);
+    
+    
+    
+    
+    
+    // int i=0;
+    // switch(i)
+    // {
+    //     case '0':printf("h");
+    //     break;
+    //     case '1':printf("p");
+    //     break;
+    //     default:printf("l");
+    // }
+    
+    
+    
+    
+    // int i;
+    
+    // for(i=0;i<5;++i++)
+    // {
+    //     printf("he");
+    // }
+    
+    
+    
+//     int x,y;
+    
+//     x=2;
+//     y=++x * ++x;
+//     printf("%d %d",x,y);
+    
+//     x=2;
+//     y=x++ * ++x;
+    
+//   printf("%d %d",x,y);
+
+
+
+
+// int x=97;
+
+// int y=sizeof(x++);
+// printf("x is %d",x);
+
+
+
+// char *p="deltax";
+// char c;
+// for(int i=0;i<3;i++)
+// {
+//     c=*p++;
+// }
+
+// printf("%c",c);
+
+
+// int a=100,b=200,c=300;
+//     if(!a>=500)
+//     b=300;
+//     c=400;
+//     cout<<a<<b<<c;
+    
+}
